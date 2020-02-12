29a5dfa87b96def666272ba4d42cf801b60dc652
Florian A <sicilia04@users.noreply.github.com>
2020-02-12 15:48:20 +0100

    Update default email

--- config/settings/base.py
+++ config/settings/base.py
@@ -224,7 +224,7 @@ EMAIL_TIMEOUT = 5
 # Django Admin URL.
 ADMIN_URL = "admin/"
 # https://docs.djangoproject.com/en/dev/ref/settings/#admins
-ADMINS = [("""Florian Alu""", "alu@prolibre.com")]
+ADMINS = [("""Florian Alu""", "fla@grapps.fr")]
 # https://docs.djangoproject.com/en/dev/ref/settings/#managers
 MANAGERS = ADMINS
 
